c9905f3d3ee6342f089263fe0bfd5f6aa89ac452
Martin Matovu <matovius@proton.me>
2024-10-25 20:39:22 +0300

    feature: create basic app setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+	import Header from "$lib/components/Header.svelte";
+  import TaskList from "$lib/components/TaskList.svelte";
+
 </script>
 
 <svelte:head>
   <title>Forgotten - Don't forget your tasks</title>
 </svelte:head>
 
+<Header />
+
+<main>
+  <div class="container">
+    <TaskList />
+  </div>
+</main>
+
 <style>
+  main {
+    padding-inline: 20px;
+
+    & > div.container {
+      padding-block: 20px;
+    }
+  }
 </style>
