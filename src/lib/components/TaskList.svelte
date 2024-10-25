d2f4c233bee7265e57d5974e1c409d3c7d432d4c
Martin Matovu <matovius@proton.me>
2024-10-25 20:38:40 +0300

    add component

---
+++ src/lib/components/TaskList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { tasks } from "$lib/scripts/stores.svelte";
+
+</script>
+
+<ul class="task-list">
+  {#each tasks.list as task}
+    <div class="task">
+      <p>{task.name}</p>
+    </div>
+  {/each}
+</ul>
