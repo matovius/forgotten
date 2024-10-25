695f3ff7c2aacb618d4a8bbd638fd2442f0898f4
Martin Matovu <matovius@proton.me>
2024-10-25 16:04:57 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "@fontsource-variable/jetbrains-mono";
+  import "@fontsource-variable/martian-mono";
+  import "../app.css";
+</script>
+
+<div class="app">
+  <slot />
+</div>
+
+<style>
+  .app {
+    isolation: isolate;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>Forgotten - Don't forget your tasks</title>
+</svelte:head>
+
+<style>
+</style>
