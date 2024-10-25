ed2aa2358d8f9434f0d27eaaf0067cfdc1e0e3fe
Martin Matovu <matovius@proton.me>
2024-10-25 19:39:42 +0300

    feature: add effect that updates localStorage with every new task.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
   import "@fontsource-variable/jetbrains-mono";
   import "@fontsource-variable/martian-mono";
   import "../app.css";
+	import { browser } from "$app/environment";
+	import { tasks } from "$lib/scripts/stores.svelte";
+
+  $effect(() => {
+    if (browser) {
+      localStorage.setItem("current-tasks", JSON.stringify(tasks.list));
+    }
+  });
 </script>
 
 <div class="app">
